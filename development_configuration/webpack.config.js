f49f22332a8739869e40e17df354fb4418d30d2b
IDeepspace <cxin1427@gmail.com>
2020-05-12 17:23:15 +0800

    add development env configuration

---
+++ development_configuration/webpack.config.js
@@ -0,0 +1,126 @@
+/*
+  - 开发环境配置：
+    - 运行项目指令：
+      - webpack 会将打包结果输出出去
+      - npx webpack-dev-server 只会在内存中编译打包，没有输出
+*/
+
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          // 预设：指示babel做怎么样的兼容性处理
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                // 按需加载
+                useBuiltIns: 'usage',
+                // 指定core-js版本
+                corejs: {
+                  version: 3
+                },
+                // 指定兼容性做到哪个版本浏览器
+                targets: {
+                  chrome: '60',
+                  firefox: '60',
+                  ie: '9',
+                  safari: '10',
+                  edge: '17'
+                }
+              }
+            ]
+          ]
+        }
+      },
+      // 处理less
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      // 处理css
+      {
+        test: /\.css$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                // postcss的插件
+                require('postcss-preset-env')()
+              ]
+            }
+          }]
+      },
+      // 处理image
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          // 关闭es6模块化
+          esModule: false,
+          outputPath: 'images'
+        }
+      },
+      // 处理html中img资源
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      // 处理其他资源
+      {
+        exclude: /\.(html|js|css|less|jpe?g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // 对输出的css文件进行重命名 不配置默认为main.css
+      filename: 'css/built.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // 压缩html代码
+      minify: {
+        // 移除空格
+        collapseWhitespace: true,
+        // 移除注释
+        removeComments: true
+      }
+    }),
+    // 压缩 css
+    new OptimizeCssAssetsWebpackPlugin()
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    open: true
+  }
+}

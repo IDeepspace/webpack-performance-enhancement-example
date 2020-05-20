44bbed963001d2efb659215d7a8ff3ea3d560a5b
IDeepspace <cxin1427@gmail.com>
2020-05-20 19:07:38 +0800

    update

--- code-splitting/demo3/webpack.config.js
+++ code-splitting/demo3/webpack.config.js
@@ -18,14 +18,5 @@ module.exports = {
       }
     })
   ],
-  /*
-    1. 可以将node_modules中代码单独打包一个chunk最终输出
-    2. 自动分析多入口chunk中，有没有公共的文件。如果有会打包成单独一个chunk
-  */
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
   mode: 'production'
 }
